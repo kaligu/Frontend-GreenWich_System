10b7e0ba80551d49336566ee76a553696d9615a9
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2023-07-28 17:55:43 +0530

    implementing member servelet

--- js/Login-Script.js
+++ js/Login-Script.js
@@ -74,3 +74,48 @@ $('#btnLogout').click(function () {
     document.querySelector(".user-dashboard").style.display="none";
 });
 
+$('#btnMemberForm').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#DasboardForm").style.display="none";
+    document.querySelector("#OrderDetailsForm").style.display="none";
+    document.querySelector("#placeOrderForm").style.display="none";
+    document.querySelector("#MemberForm").style.display="block";
+    document.querySelector("#itemForm").style.display="none";
+});
+
+$('#btnItemForm').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#DasboardForm").style.display="none";
+    document.querySelector("#OrderDetailsForm").style.display="none";
+    document.querySelector("#placeOrderForm").style.display="none";
+    document.querySelector("#itemForm").style.display="block";
+    document.querySelector("#MemberForm").style.display="none";
+});
+
+$('#btnPlaceorderForm').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#DasboardForm").style.display="none";
+    document.querySelector("#OrderDetailsForm").style.display="none";
+    document.querySelector("#placeOrderForm").style.display="block";
+    document.querySelector("#itemForm").style.display="none";
+    document.querySelector("#MemberForm").style.display="none";
+});
+
+$('#btnOrderDetailsForm').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#DasboardForm").style.display="none";
+    document.querySelector("#OrderDetailsForm").style.display="block";
+    document.querySelector("#placeOrderForm").style.display="none";
+    document.querySelector("#itemForm").style.display="none";
+    document.querySelector("#MemberForm").style.display="none";
+});
+
+$('#logoName').click(function() {
+    // Load the card.html file into the #card-container div
+    document.querySelector("#DasboardForm").style.display="block";
+    document.querySelector("#OrderDetailsForm").style.display="none";
+    document.querySelector("#placeOrderForm").style.display="none";
+    document.querySelector("#itemForm").style.display="none";
+    document.querySelector("#MemberForm").style.display="none";
+});
+
---
+++ js/Member-Script.js
@@ -0,0 +1,106 @@
+//add member
+$('#addMemberSaveBtn').click(function() {
+    const membernic = $('#memberAddNIC').val();
+    const membername = $('#memberAddName').val();
+    const membertell = $('#memberAddTell').val();
+    const memberaddress = $('#memberAddAddress').val();
+
+    const newMember = {membernic, membername, membertell, memberaddress};
+
+    const newMemberJson = JSON.stringify(newMember);
+
+    console.log(newMemberJson);
+
+// Call the function to send the AJAX request
+    createAjaxReqMember(newMemberJson, "POST")
+        .then(function (result) {
+            // Handle the AJAX request success or failure here
+            if (result.includes("new member added")) {
+                $('#memberAddNIC').val('');
+                $('#memberAddName').val('');
+                $('#memberAddTell').val('');
+                $('#memberAddAddress').val('');
+
+                alert("New Member Added Successfully!");
+
+                console.log('AJAX request succeeded');
+            } else {
+
+                alert("Try Again!");
+
+                console.error('*AJAX request failed');
+            }
+        })
+        .catch(function (error) {
+            alert("Try Again!");
+
+            console.error('AJAX request failed:', error);
+        });
+
+});
+
+//update member
+$('#updateMemberUpdateBtn').click(function() {
+    const membernic = $('#memberUpdateNIC').val();
+    const membername = $('#memberUpdateName').val();
+    const membertell = $('#memberUpdateTell').val();
+    const memberaddress = $('#memberUpdateAddress').val();
+
+    const updateMember = {membernic, membername, membertell, memberaddress};
+
+    const updateMemberJson = JSON.stringify(updateMember);
+
+    console.log(updateMemberJson);
+
+// Call the function to send the AJAX request
+    createAjaxReqMember(updateMemberJson, "PUT")
+        .then(function (result) {
+            // Handle the AJAX request success or failure here
+            if (result.includes("member updated")) {
+                $('#memberUpdateNIC').val('');
+                $('#memberUpdateName').val('');
+                $('#memberUpdateTell').val('');
+                $('#memberUpdateAddress').val('');
+
+                alert("Member Updated Successfully!");
+
+                console.log('AJAX request succeeded');
+            } else {
+
+                alert("Try Again!");
+
+                console.error('*AJAX request failed');
+            }
+        })
+        .catch(function (error) {
+            alert("Try Again!");
+
+            console.error('AJAX request failed:', error);
+        });
+
+});
+
+createAjaxReqMember = (MemberJson, method) => {
+    return new Promise((resolve, reject) => {
+        // AJAX request
+        const http = new XMLHttpRequest();
+
+        http.onreadystatechange = () => {
+            if (http.readyState === XMLHttpRequest.DONE) {
+                console.log("before state")
+                if (http.status === 200) {
+                    console.log("after state")
+                    const response = http.responseText;
+                    console.log(response)
+                    resolve(response); // AJAX response return
+                } else {
+                    resolve(false); // AJAX request failed
+                }
+            }
+        };
+
+        http.open(method, "http://localhost:8080/greenwich/Member-Manage", true);
+        http.setRequestHeader('Content-Type', 'application/json');
+        http.send(MemberJson);
+    });
+}
